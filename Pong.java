504684ec9c0beecd8479e1ba9ff3ed0f4d3f2db7
nfichter <nfichter@stuy.edu>
2016-05-13 17:45:03 -0400

    Began making actual pong game

--- Pong.java
+++ Pong.java
@@ -8,29 +8,65 @@ import java.io.InputStream;
 //http://stackoverflow.com/questions/9545388/how-can-i-detect-arrow-keys-in-java-console-not-in-gui
 //https://docs.oracle.com/javase/tutorial/essential/exceptions/finally.html
 
-public class Snake {
+public class Pong {
+    private char[][] board;
+    private int pos1;
+    private int pos2;
+    private int difficulty;
+    
+    public Pong(int difficulty) {
+        //initialize the board to by an empty 9 by 25 board
+        board = new char[9][25];
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                board[r][c] = ' ';
+            }
+        }
+        //create the paddles at each end of the board
+        pos1 = 3;
+        pos2 = 3;
+        for (int i = pos1; i < pos1+3; i++) {
+            board[0][i] = '#';
+        }
+        for (int i = pos2; i < pos2+3; i++) {
+            board[board.length-1][i] = '#';
+        }
+        board[board.length/2][board[0].length/2] = '*';
+        
+        this.difficulty = difficulty; //set difficulty
+    }
+    
+    public void move(int key) {
+        if (key == 0x77) {
+            board[0][pos1-1] = '#';
+            board[0][pos1+3] = ' ';
+            pos1++;
+        }
+        if (key == 0x73) {
+            board[0][pos1] = ' ';
+            board[0][pos1+4] = '#';
+            pos1++;
+        }
+    }
+    
     //Thanks to Graham King from darkcoding.net for the lesson on making the terminal interactive
     private static String ttyConfig;
 
     public static void main(String[] args) {
 	try {
 	    setTerminalToCBreak();
-	    int i = 0;
 	    while (true) {
 		if (System.in.available() != 0) { //if a button is pressed:
-		    int exitKey = System.in.read();
+		    int key = System.in.read();
 		    System.out.println(exitKey);
-		    if (exitKey == 0x48 || exitKey == 0x61) { //if the button pressed is the up key:
-			break; //stop the loop
-		    }
+		    move(key);
 		}
-		i++;
 	    }
 	} catch (IOException e) {
 	    System.out.println("IOException");
 	} catch (InterruptedException e) {
 	    System.out.println("InterruptedException");
-	}
+    }
 	finally {
 	    try {
 		stty(ttyConfig.trim());
