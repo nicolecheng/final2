90e2b23d6f6067267b690853cd4c4503e6deedd5
Noah Fichter <nfichter@stuy.edu>
2016-05-13 12:19:43 -0400

    Almost working terminal input, need to figure out arrow keys as input - hex codes are weird

---
+++ Snake.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+//Resources
+//https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+//http://www.darkcoding.net/software/non-blocking-console-io-is-not-possible/
+//http://stackoverflow.com/questions/9545388/how-can-i-detect-arrow-keys-in-java-console-not-in-gui
+//https://docs.oracle.com/javase/tutorial/essential/exceptions/finally.html
+
+public class Snake {
+    //Thanks to Graham King from darkcoding.net for the lesson on making the terminal interactive
+    private static String ttyConfig;
+
+    public static void main(String[] args) {
+	try {
+	    setTerminalToCBreak();
+	    int i = 0;
+	    while (true) {
+		if (System.in.available() != 0) { //if a button is pressed:
+		    int exitKey = System.in.read();
+		    System.out.println(exitKey);
+		    if (exitKey == 0x48 || exitKey == 0x61) { //if the button pressed is the up key:
+			break; //stop the loop
+		    }
+		}
+		i++;
+	    }
+	} catch (IOException e) {
+	    System.out.println("IOException");
+	} catch (InterruptedException e) {
+	    System.out.println("InterruptedException");
+	}
+	finally {
+	    try {
+		stty(ttyConfig.trim());
+	    } catch (Exception e) {
+		System.out.println("Exception restoring tty config");
+	    }
+	}
+    }
+
+    private static void setTerminalToCBreak() throws IOException, InterruptedException { //used in main()
+	ttyConfig = stty("-g");
+	stty("-icanon min 1"); //makes the console go character by character rather than line by line
+	stty("-echo"); //disables the terminal displaying the character pressed
+    }
+
+    private static String stty(final String args) throws IOException, InterruptedException { //used in setTerminalToCBreak()
+	String cmd = "stty " + args + " < /dev/tty";
+	
+	return exec(new String[] {
+		"sh",
+		"-c",
+		cmd
+	    });
+    }
+
+    private static String exec(final String[] cmd) throws IOException, InterruptedException { //used in stty()
+	ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+	Process p = Runtime.getRuntime().exec(cmd);
+        int c;
+        InputStream in = p.getInputStream();
+
+	while ((c = in.read()) != -1) {
+            bout.write(c);
+        }
+
+        in = p.getErrorStream();
+
+        while ((c = in.read()) != -1) {
+            bout.write(c);
+        }
+	
+        p.waitFor();
+
+        String result = new String(bout.toByteArray());
+        return result;
+    }
+}
