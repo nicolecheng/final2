a1d51d485ae34c482fffc187adccf540f7679ce2
nicolecheng <ncheng@stuy.edu>
2016-06-05 23:16:13 -0400

    update

--- Doggie.java
+++ Doggie.java
@@ -13,14 +13,26 @@ public class Doggie{
     private static String dog1 = "         ,-~~~~-,\n   .-~~~:        :~~~-.\n  !    !          !    !\n {   .^{  O    O  }^.   }\n  ^~^  { .-~~~~-. }  ^~^\n       :!        !:\n      !^._  ()  _.^!\n     !    ^~~~~^    !\n    :                :\n    {                }\n    {     }    {     }\n    {     }    {     }\n    l     l    l     l\n   { { {   }~~{   } } }\n    ^~~~~~^    ^~~~~~^\n";
     private static String dog2 = "    ___\n __/o |`.  ..---.   )\n |_,  |_|-'  /   )-^\n    )        |  ((\n ___Y  ,    .-7 /|\n(_,___/...-- (_/_/\n";
     private static String[]mysteryBasket;
+    private static String[][]foods;
 
     public Doggie(){
         days=0;
 	hours=0;
 	hunger=100;
 	bored=100;
-	tired=0;
+	tired=50;
 	name="";
+	foods=new String[5][2]foods;
+	foods[0][0]="corn";
+	foods[0][1]="corndog";
+	foods[1][0]="waffles";
+	foods[1][1]="woofle";
+	foods[2][0]="popsicles";
+	foods[2][1]="pupsicle";
+	foods[3][0]="poached eggs";
+	foods[3][1]="pooched egg";
+	foods[4][0]="popcorn";
+	foods[4][1]="pupcorn";
     }
 
     private static void intro(){
@@ -74,11 +86,14 @@ public class Doggie{
 	while(days<=7){
 	    System.out.println(dog1);
 	    System.out.println("\nName: "+name+"   Days: "+days+" days   Hours: "+hours+" hours   Hunger: "+hunger+"   Bored: "+bored+"   Tired: "+tired);   
+	    if(hours%3==0){
+		System.out.println();
+		instruct();
+	    }
 	    System.out.println();
-	    instruct();
-	    System.out.println();
-	    System.out.println();
-	    System.out.println();
+	    wait(2500);
+	    hours+=1;
+	    //System.out.println();
 	}
     }
 
@@ -89,18 +104,98 @@ public class Doggie{
 	Scanner in = new Scanner(System.in);
 	String n = in.nextLine();
 	if(n.contains("1") || n.contains("feed")){
-	    feed();
+	    if(feed()){
+		bored+=20;
+		tired+=20;
+		hours+=1;
+	    }
 	}else if(n.contains("2") || n.contains("play")){
-	    play();
+	    if(play()){
+		hunger+=30;
+		tired+=40;
+		hours+=1;
+	    }
 	}else if(n.contains("3") || n.contains("tuck") || n.contains("sleep")){
-	    tuck();
+	    if(tuck()){
+		hunger+=30;
+		hours+=1;
+	    }
 	}else if(n.contains("4") || n.contains("mystery")){
-	    mystery();
+	    if(mystery()){
+		hours+=1;
+	    }
 	}else{
 	    System.out.println("Sorry, what was that?\n");
 	    wait(1500);
 	    instruct();
 	}
+    }
+
+    private static boolean feed(){
+	System.out.println();
+	if(hunger<=0){
+	    System.out.println("Chill. "+name+" doesn't get hungry THAT often...");
+	    return false;
+	}else{
+	    System.out.println("What would you like to feed your puppy pal?\n");
+	    System.out.println("1) kibble\t2) puppy chow\t3) steak\t4) random (again, careful)");
+	    Scanner in = new Scanner(System.in);
+	    String n = in.nextLine();
+	    if(n.contains("1") || n.contains("kibble")){
+		System.out.println(name+" nibbles on the kibble. Delicious.");
+		if(hunger>=30){
+		    hunger-=30;
+		}else{
+		    hunger=0;
+		}
+	    }else if(n.contains("2") || n.contains("chow")){
+		System.out.println(name+" chows down on the chow. Yum.");
+		if(hunger>=40){
+		    hunger-=40;
+		}else{
+		    hunger=0;
+		}
+	    }else if(n.contains("3") || n.contains("steak")){
+		System.out.println(name+" is overjoyed with this meal, reassuring you that feeding your dog a gourmet steak was not a mis-steak.");
+		if(hunger>=50){
+		    hunger-=50;
+		}else{
+		    hunger=0;
+		}
+	    }else if(n.contains("4") || n.contains("random")){
+		int i = (int)(Math.random()*5);
+		System.out.println("You decide to feed "+name+" some "+foods[i][0]+".\n");
+		wait(2000);
+		System.out.println("Oh, look: "+name+" turned into a "+foods[i][1]+".\n");
+		wait(2000);
+		System.out.println("That's unfortunate.");
+		if(hunger>=20){
+		    hunger-=20;
+		}else{
+		    hunger=0;
+		}
+	    }else{
+		System.out.println("Sorry, what was that?");
+		wait(1000);
+		feed();
+	    }
+	    return true;
+	}
+    }
+
+    private static boolean play(){
+
+    }
+
+    private static boolean tuck(){
+	if(tired<0=){
+	    System.out.println("No. "+name+" refuses to go to bed. "+name+" is not tired at all.");
+	    return false;
+	}
+    }
+
+    private static boolean mystery(){
+
     }
 
     private static void nameDog(){
